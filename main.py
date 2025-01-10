5701002606dcebe2637f67712ebbf6b31188dc29
Katelyn Curtiss <153852200+KatelynCurtiss@users.noreply.github.com>
2025-01-10 10:50:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,25 @@
 # Quiz
 
 def square_number(num1):
-    return square_number
+    return num1 ** 2
+    #return num1 * num1
 
 def add_squares(num1, num2):
-    add_squares()
-    add_squares()
-    return(add_squares)
+    # add_squares()
+    # add_squares()
+    num1_squared = square_number(num1)
+    num2_squared = square_number(num2)
+    total = num1_squared + num2_squared
+    return total
 
 def main():
-    num1 = (input("Enter the first number: "))
-    num2 = (input("Enter the second number: "))
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
 
-    add_squares()
+    result = add_squares(num1, num2)
 
-    result = main 
+    print(f'The sum of the squares of {num1} and {num2} is: {result}')
+
+
+if __name__ == '__main__':
+    main()
