d62364455d23df18a7ec4de3da39424fca29d6f2
Katelyn Curtiss <153852200+KatelynCurtiss@users.noreply.github.com>
2025-01-10 10:24:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+# Katelyn Curtiss
+# January 10 2024
+# Quiz
 
+def square_number(num1):
+    return square_number
+
+def add_squares(num1, num2):
+    add_squares()
+    add_squares()
+    return(add_squares)
+
+def main():
+    num1 = (input("Enter the first number: "))
+    num2 = (input("Enter the second number: "))
+
+    add_squares()
+
+    result = main 
